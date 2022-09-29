a95eb2de9bdafdc3e43bff606c9deb7a33ec73c4
Le Huynh Duc <lehuynhduc29a1@gmail.com>
2022-09-29 09:43:49 +0700

    configure dependency injection

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,18 +1,68 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get_it/get_it.dart';
+import 'package:number_trivia/core/network/network_info.dart';
+import 'package:number_trivia/core/util/input_converter.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_data_source.dart';
+import 'package:number_trivia/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:number_trivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'features/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+import 'features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:http/http.dart' as http;
 
 final sl = GetIt.instance;
 
 Future<void> configureDependencies() async {
   //! Features - Number Trivia
+  _initFeature();
+
+  //! Core
+  _initCore();
+
+  //! External
+  await _initExternal();
+}
+
+void _initFeature() {
   // Bloc
   sl.registerFactory(
     () => NumberTriviaBloc(concrete: sl(), random: sl(), inputConverter: sl()),
   );
 
-  //! Core
+  // Use cases
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
 
-  //! External
+  // Repository
+  sl.registerLazySingleton<NumberTriviaRepository>(
+    () => NumberTriviaRepositoryImpl(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInfo: sl(),
+    ),
+  );
+
+  // Data sources
+  sl.registerLazySingleton<NumberTriviaRemoteDataSource>(
+    () => NumberTriviaRemoteDataSourceImpl(client: sl()),
+  );
+  sl.registerLazySingleton<NumberTriviaLocalDataSource>(
+    () => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()),
+  );
 }
 
-void _initFeature() {}
+void _initCore() {
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+}
+
+Future<void> _initExternal() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+
+  sl.registerLazySingleton(() => sharedPreferences);
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => Connectivity());
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:number_trivia/features/number_trivia/presentation/pages/number_trivia_app.dart';
 
-import 'injection_container.dart';
+import 'injection_container.dart' as di;
 
 Future<void> main() async {
-  await configureDependencies();
+  await di.configureDependencies();
 
   runApp(const NumberTriviaApp());
 }
